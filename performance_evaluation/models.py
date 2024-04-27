0df988b5b76aaf91749b58b0057f3f177f453e6c
Naveen Sargam <naveensargam123@gmail.com>
2024-04-27 21:37:02 +0530

    models are created and its migrations applied

--- performance_evaluation/models.py
+++ performance_evaluation/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class VendorPerformanceRecord(models.Model):
+    vendor = models.ForeignKey('profile_management.Vendor', on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    on_time_delivery_rate = models.FloatField()
+    quality_rating_avg = models.FloatField()
+    average_response_time = models.FloatField()
+    fulfillment_rate = models.FloatField()
+
+    def __str__(self):
+        return f"{self.vendor.name} - {self.date}"
--- purchase_order_tracking/models.py
+++ purchase_order_tracking/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class PurchaseOrder(models.Model):
+    po_number = models.CharField(max_length=50, unique=True)
+    vendor = models.ForeignKey('profile_management.Vendor', on_delete=models.CASCADE)
+    order_date = models.DateTimeField()
+    delivery_date = models.DateTimeField()
+    items = models.JSONField()
+    quantity = models.IntegerField()
+    status = models.CharField(max_length=50)
+    quality_rating = models.FloatField(null=True)
+    issue_date = models.DateTimeField()
+    acknowledgment_date = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return self.po_number
